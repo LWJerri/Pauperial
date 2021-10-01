c93591740017cef15f9e09445705600268d757ab
Satont <satontworldwide@gmail.com>
2021-10-01 21:11:49 +0300

    Add typings for generateQr

--- frontend/src/helpers/qr.ts
+++ frontend/src/helpers/qr.ts
@@ -1,6 +1,6 @@
 import qr from "qrcode";
 
-export const generateQr = (link: string) => {
+export const generateQr = (link: string): Promise<string> => {
   return new Promise((resolve, reject) => {
     qr.toDataURL(link, (err, url) => {
       if (err) reject(err);
