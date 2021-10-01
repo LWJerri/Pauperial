eb4d49265822c7f160b9dfc0e9820a928465c522
LWJerri <CLWJerri@yandex.ua>
2021-10-01 16:43:12 +0300

    Add typeorm entities

---
+++ backend/src/typeorm/entities/Links.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Links {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  code: string;
+
+  @Column({ nullable: false })
+  link: string;
+
+  @Column({ nullable: false, default: 0 })
+  codeUses: number;
+}
