93e448f1185b006f831f5bd75eaa66bc27679fbf
Satont <satontworldwide@gmail.com>
2021-10-01 19:05:09 +0300

    Add secret for links

--- frontend/src/components/Redirect.svelte
+++ frontend/src/components/Redirect.svelte
@@ -2,17 +2,24 @@
   import { onMount } from "svelte";
 
   export let code;
+  let response = 'Wait...'
   const API_URL = "http://localhost:4000/code/";
 
   onMount(async () => {
-    const res = await fetch(`${API_URL}${code}`, { method: "GET" });
-    const respData = await res.json();
-
-    window.location.href = respData.link ?? window.location.origin;
+    try {
+      const params = (new URL(document.location.href)).searchParams;
+      const res = await fetch(`${API_URL}${code}?secret=${params.get('secret') || ''}`, { method: "GET" });
+      const respData = await res.json();
+  
+      window.location.href = respData.link ?? window.location.origin;
+    } catch (e) {
+      e = await e.json()
+      response = e.error
+    }
   });
 </script>
 
-<p>Wait...</p>
+<p>{{ response }}</p>
 
 <style>
   p {
