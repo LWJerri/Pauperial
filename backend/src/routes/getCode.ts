384f85af1ec1d3b2955a11f3c22e156965c76afb
LWJerri <CLWJerri@yandex.ua>
2021-10-01 16:44:04 +0300

    Finish getCode route

---
+++ backend/src/routes/getCode.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Links } from "../typeorm/entities/Links";
+
+export default async function getCode(req: Request, res: Response) {
+  try {
+    const errResp = { code: req.params.id, link: null, views: null };
+    const codeRepository = getRepository(Links);
+    const findCode = await codeRepository.findOne({ code: req.params.id });
+
+    if (!findCode) return res.status(400).json(errResp);
+
+    res.status(200).json({ code: req.params.id, link: findCode.link, views: findCode.codeUses });
+
+    findCode.codeUses = findCode.codeUses + 1;
+    return codeRepository.save(findCode);
+  } catch (err) {
+    console.error("getCode error:", err);
+
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
