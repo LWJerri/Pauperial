b17be567b4d6b043bb0329fb15e35d284882383d
LWJerri <CLWJerri@yandex.ua>
2021-10-01 16:44:19 +0300

    Finish untracked route

---
+++ backend/src/routes/unknown.ts
@@ -0,0 +1,11 @@
+import type { Request, Response } from "express";
+
+export default async function unknown(req: Request, res: Response) {
+  try {
+    return res.status(404).json({ message: "Endpoint not found!" });
+  } catch (err) {
+    console.error("unknown error:", err);
+
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
