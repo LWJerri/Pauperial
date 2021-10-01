44070c13cc0fa681449e6487f493a3063a13f106
LWJerri <CLWJerri@yandex.ua>
2021-10-01 16:40:55 +0300

    Main svelte file

---
+++ frontend/src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-navigator";
+  import Main from "./components/Main.svelte";
+  import Redirect from "./components/Redirect.svelte";
+</script>
+
+<Router>
+  <main>
+    <Route path="/:code" component={Redirect} />
+    <Route component={Main} />
+  </main>
+</Router>
+
+<style>
+  :root {
+    background-color: #23272a;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+</style>
