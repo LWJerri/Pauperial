75b51152ec585239ee39344943f9db82e06c7658
LWJerri <CLWJerri@yandex.ua>
2021-10-02 21:35:22 +0300

    Prettier rules

--- backend/src/typeorm/migrations/1633088284008-AddedLinks.ts
+++ backend/src/typeorm/migrations/1633088284008-AddedLinks.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AddedLinks1633088284008 implements MigrationInterface {
-    name = 'AddedLinks1633088284008'
+  name = "AddedLinks1633088284008";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "links" ("id" SERIAL NOT NULL, "code" character varying NOT NULL, "link" character varying NOT NULL, "codeUses" integer NOT NULL DEFAULT '0', CONSTRAINT "PK_ecf17f4a741d3c5ba0b4c5ab4b6" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "links" ("id" SERIAL NOT NULL, "code" character varying NOT NULL, "link" character varying NOT NULL, "codeUses" integer NOT NULL DEFAULT '0', CONSTRAINT "PK_ecf17f4a741d3c5ba0b4c5ab4b6" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "links"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "links"`);
+  }
 }
--- backend/src/typeorm/migrations/1633108605359-add_secrets.ts
+++ backend/src/typeorm/migrations/1633108605359-add_secrets.ts
@@ -1,14 +1,13 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class addSecrets1633108605359 implements MigrationInterface {
-    name = 'addSecrets1633108605359'
+  name = "addSecrets1633108605359";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."links" ADD "secret" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "public"."links" ADD "secret" character varying`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."links" DROP COLUMN "secret"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "public"."links" DROP COLUMN "secret"`);
+  }
 }
