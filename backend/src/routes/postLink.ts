f4830f53b095b9ff0f37737dd514c14a2ae152d2
LWJerri <CLWJerri@yandex.ua>
2021-10-02 21:38:44 +0300

    Display "error" field in JSON output and add QR generator

--- backend/src/routes/postLink.ts
+++ backend/src/routes/postLink.ts
@@ -1,31 +1,23 @@
 import type { Request, Response } from "express";
 import { nanoid } from "nanoid";
 import { Links } from "../typeorm/entities/Links";
-import { URL } from "url";
 import { getRepository } from "typeorm";
-
-const isLinkValid = (link: string) => {
-  try {
-    new URL(link);
-    return true;
-  } catch (err) {
-    return false;
-  }
-};
+import linkValidator from "../helpers/linkValidator";
+import generateQR from "../helpers/qr";
 
 export async function postLink(req: Request, res: Response) {
   try {
-    if (!req.body.link || !isLinkValid(req.body.link)) {
-      return res.status(400).json({ code: null, link: req.body.link, views: null, message: 'You pasted not valid link.' })
-    };
+    if (!req.body.link || !linkValidator(req.body.link)) return res.status(400).json({ error: true, message: "You pasted not valid link" });
 
     const codeRepository = getRepository(Links);
-    const link = await codeRepository.save({ code: nanoid(10), link: req.body.link, codeUses: 0, secret: req.body.secret });
+    const code = nanoid(10);
+    const qrLink = await generateQR(new URL(`${req.body.origin}/${code}${req.body.secret ? `?secret=${req.body.secret}` : ""}`).toString());
+    const link = await codeRepository.save({ code: code, link: req.body.link, views: 0, secret: req.body.secret, qrData: qrLink });
 
-    return res.status(200).json({ ...link, views: link.codeUses });
+    return res.status(200).json({ error: false, ...link });
   } catch (err) {
     console.error("postLink error:", err);
 
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ error: true, message: "Internal Server Error" });
   }
 }
