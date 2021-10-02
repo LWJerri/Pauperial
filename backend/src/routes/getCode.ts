821fec27a475b581e54e4886cd6cf031bc5fb18b
LWJerri <CLWJerri@yandex.ua>
2021-10-03 00:03:54 +0300

    Update getCode function

--- backend/src/routes/getCode.ts
+++ backend/src/routes/getCode.ts
@@ -1,22 +1,18 @@
 import type { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import getLink from "../helpers/getLink";
 import { Links } from "../typeorm/entities/Links";
 
 export async function getCode(req: Request, res: Response) {
   try {
-    const linkRepository = getRepository(Links);
-    const link = await linkRepository.findOne({ code: req.params.id });
+    const link = await getLink(req.params.id, req.headers.host);
+    getRepository(Links).update({ code: req.params.id }, { views: link.views++ });
 
     if (!link) return res.status(400).json({ error: true, message: "Code not found" });
     if (link.secret && req.query.secret !== link.secret)
       return res.status(403).json({ error: true, message: "Wrong secret for that link" });
 
-    res
-      .status(200)
-      .json({ error: false, code: req.params.id, link: link.link, views: link.views, secret: link.secret, qrData: link.qrData });
-
-    link.views = link.views + 1;
-    return linkRepository.save(link);
+    return res.status(200).json({ error: false, ...link });
   } catch (err) {
     console.error("getCode error:", err);
 
