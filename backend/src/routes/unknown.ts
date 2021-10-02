a9e7ad9b75823878c34c70aea20faaaa8f615645
LWJerri <CLWJerri@yandex.ua>
2021-10-02 21:38:15 +0300

    Display "error" field in JSON output

--- backend/src/routes/unknown.ts
+++ backend/src/routes/unknown.ts
@@ -2,10 +2,10 @@ import type { Request, Response } from "express";
 
 export async function unknown(req: Request, res: Response) {
   try {
-    return res.status(404).json({ message: "Endpoint not found!" });
+    return res.status(404).json({ error: true, message: "Endpoint not found!" });
   } catch (err) {
     console.error("unknown error:", err);
 
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ error: true, message: "Internal Server Error" });
   }
 }
