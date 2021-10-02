5d5c25f0c18bb29dc2076dd776801db23fe9a9d2
Satont <satontworldwide@gmail.com>
2021-10-02 23:06:49 +0300

    Host frontend via backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,15 +3,21 @@ import "reflect-metadata";
 import cors from "cors";
 import express from "express";
 import boot from "./helpers/boot";
-import { getCode, postLink, unknown } from "./routes";
+import { getCode, postLink } from "./routes";
+import { join } from "path";
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors(), express.json());
 
-app.get("/code/:id", async (req, res) => getCode(req, res));
-app.post("/code", async (req, res) => postLink(req, res));
-app.get("*", async (req, res) => unknown(req, res));
+app.use(express.static(join(__dirname, '..', '..', 'frontend', 'dist')));
+
+app.get("/api/code/:id", async (req, res) => getCode(req, res));
+app.post("/api/code", async (req, res) => postLink(req, res));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'))
+})
 
 app.listen(port, async () => boot(port));
