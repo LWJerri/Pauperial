9ab8f72996c3df44008a1f96ad4fe72dbf1c16df
LWJerri <CLWJerri@yandex.ua>
2021-10-02 21:39:14 +0300

    Display "error" field in JSON output and update error messages

--- backend/src/routes/getCode.ts
+++ backend/src/routes/getCode.ts
@@ -4,22 +4,22 @@ import { Links } from "../typeorm/entities/Links";
 
 export async function getCode(req: Request, res: Response) {
   try {
-    const errResp = { code: req.params.id, link: null, views: null };
     const linkRepository = getRepository(Links);
     const link = await linkRepository.findOne({ code: req.params.id });
 
-    if (!link) return res.status(400).json(errResp);
-    if (link.secret && req.query.secret !== link.secret) {
-      return res.status(403).json({ message: 'Wrong secret for that link' })
-    }
+    if (!link) return res.status(400).json({ error: true, message: "Code not found" });
+    if (link.secret && req.query.secret !== link.secret)
+      return res.status(403).json({ error: true, message: "Wrong secret for that link" });
 
-    res.status(200).json({ code: req.params.id, link: link.link, views: link.codeUses });
+    res
+      .status(200)
+      .json({ error: false, code: req.params.id, link: link.link, views: link.views, secret: link.secret, qrData: link.qrData });
 
-    link.codeUses = link.codeUses + 1;
+    link.views = link.views + 1;
     return linkRepository.save(link);
   } catch (err) {
     console.error("getCode error:", err);
 
-    return res.status(500).json({ message: "Internal Server Error" });
+    return res.status(500).json({ error: true, message: "Internal Server Error" });
   }
 }
