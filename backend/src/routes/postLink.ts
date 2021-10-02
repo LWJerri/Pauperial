32f4affa8f5ea6f2dbc1e56c9d342c95b11705f4
LWJerri <CLWJerri@yandex.ua>
2021-10-03 00:03:49 +0300

    Update postLink function

--- backend/src/routes/postLink.ts
+++ backend/src/routes/postLink.ts
@@ -2,19 +2,20 @@ import type { Request, Response } from "express";
 import { nanoid } from "nanoid";
 import { Links } from "../typeorm/entities/Links";
 import { getRepository } from "typeorm";
-import linkValidator from "../helpers/linkValidator";
+import linkValidation from "../helpers/linkValidation";
 import generateQR from "../helpers/qr";
+import getLink from "../helpers/getLink";
 
 export async function postLink(req: Request, res: Response) {
   try {
-    if (!req.body.link || !linkValidator(req.body.link)) return res.status(400).json({ error: true, message: "You pasted not valid link" });
+    if (!req.body.link || !linkValidation(req.body.link))
+      return res.status(400).json({ error: true, message: "You pasted not valid link" });
 
     const codeRepository = getRepository(Links);
     const code = nanoid(10);
-    const qrLink = await generateQR(new URL(`${req.body.origin}/${code}${req.body.secret ? `?secret=${req.body.secret}` : ""}`).toString());
-    const link = await codeRepository.save({ code: code, link: req.body.link, views: 0, secret: req.body.secret, qrData: qrLink });
+    await codeRepository.save({ code: code, link: req.body.link, views: 0, secret: req.body.secret });
 
-    return res.status(200).json({ error: false, ...link });
+    return res.status(200).json({ error: false, ...(await getLink(code, req.headers.host)) });
   } catch (err) {
     console.error("postLink error:", err);
 
