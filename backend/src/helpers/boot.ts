cfdcbd9e171bfc37839625f9d1b187482322f70f
LWJerri <CLWJerri@yandex.ua>
2021-10-03 13:13:52 +0300

    Update helpers

--- backend/src/helpers/boot.ts
+++ backend/src/helpers/boot.ts
@@ -2,7 +2,7 @@ import { createConnection } from "typeorm";
 
 export default async function boot(port: string | number) {
   try {
-    console.log(` > API ready! Port - ${port}`);
+    console.log(` > Pauperial ready! Port - ${port}`);
     await createConnection();
 
     console.log(` > Connected to database!`);
--- backend/src/helpers/getLink.ts
+++ backend/src/helpers/getLink.ts
@@ -5,12 +5,17 @@ import generateQR from "./qr";
 export default async function getLink(code: string, host: string) {
   const link = await getRepository(Links).findOne({ code });
 
-  if (!link) throw new Error("Link not found");
+  try {
+    if (!link) throw new Error("Code not found");
 
-  const qrData = await generateQR(new URL(`http://${host}/${code}${link.secret ? `?secret=${link.secret}` : ""}`).toString());
+    const qrData = await generateQR(new URL(`http://${host}/${code}${link.secret ? `?secret=${link.secret}` : ""}`).toString());
 
-  return {
-    ...link,
-    qrData,
-  };
+    return {
+      error: false,
+      ...link,
+      qrData,
+    };
+  } catch (err) {
+    return { error: true, message: err.message, ...link };
+  }
 }
