4eda787cdeb7c99be98c77eeb169051b6ef7ece5
LWJerri <CLWJerri@yandex.ua>
2021-10-03 13:16:33 +0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,15 +9,12 @@ import { join } from "path";
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(cors(), express.json());
-
-app.use(express.static(join(__dirname, '..', '..', 'frontend', 'dist')));
+app.use(cors(), express.json(), express.static(join(__dirname, "..", "..", "frontend", "dist")));
 
 app.get("/api/code/:id", async (req, res) => getCode(req, res));
 app.post("/api/code", async (req, res) => postLink(req, res));
-
-app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "frontend", "dist", "index.html"));
+});
 
 app.listen(port, async () => boot(port));
