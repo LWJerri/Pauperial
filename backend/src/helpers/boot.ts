83f9bbac4d579d4606c68643e974d5b79fe0a31a
LWJerri <CLWJerri@yandex.ua>
2022-01-05 17:41:26 +0200

    Set maximum db connections

--- backend/src/helpers/boot.ts
+++ backend/src/helpers/boot.ts
@@ -1,10 +1,10 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
 export default async function boot(port: string | number) {
   try {
     console.log(`> Pauperial ready! Port - ${port}`);
 
-    await createConnection();
+    await createConnection({ ...(await getConnectionOptions()), extra: { max: 5 } });
 
     console.log(`> Connected to database!`);
   } catch (err) {
