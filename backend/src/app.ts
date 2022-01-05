f15f9b0bbe6a908b1269d2fee611cd0cfe6c7091
LWJerri <CLWJerri@yandex.ua>
2022-01-05 11:02:33 +0200

    Update backend config files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,7 +3,7 @@ import "reflect-metadata";
 import cors from "cors";
 import express from "express";
 import boot from "./helpers/boot";
-import { getCode, postLink } from "./routes";
+import { getCode, postLink, postMessage, stats } from "./routes";
 import { join } from "path";
 
 const app = express();
@@ -12,7 +12,9 @@ const port = process.env.PORT || 4000;
 app.use(cors(), express.json(), express.static(join(__dirname, "..", "..", "frontend", "dist")));
 
 app.get("/api/code/:id", async (req, res) => getCode(req, res));
+app.get("/api/stats", async (req, res) => stats(req, res));
 app.post("/api/code", async (req, res) => postLink(req, res));
+app.post("/api/message", async (req, res) => postMessage(req, res));
 app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "..", "..", "frontend", "dist", "index.html"));
 });
