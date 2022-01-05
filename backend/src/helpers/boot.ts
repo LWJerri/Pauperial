12a04c1158a72b34e4dae953bf14e84b1ffe2afa
LWJerri <CLWJerri@yandex.ua>
2022-01-05 11:01:21 +0200

    Update helpers

--- backend/src/helpers/boot.ts
+++ backend/src/helpers/boot.ts
@@ -2,10 +2,11 @@ import { createConnection } from "typeorm";
 
 export default async function boot(port: string | number) {
   try {
-    console.log(` > Pauperial ready! Port - ${port}`);
+    console.log(`> Pauperial ready! Port - ${port}`);
+
     await createConnection();
 
-    console.log(` > Connected to database!`);
+    console.log(`> Connected to database!`);
   } catch (err) {
     console.error("boot error:", err);
   }
