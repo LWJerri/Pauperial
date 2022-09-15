b6291689ac7ed1b52994df5a89c2361c0c612a4d
LWJerri <loginmanx@gmail.com>
2022-09-15 06:50:37 +0000

    Add coming soon frontend

---
+++ apps/frontend/src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Route, Router } from "svelte-navigator";
+  import Main from "./pages/Main.svelte";
+</script>
+
+<Router>
+  <main>
+    <Route component={Main} />
+  </main>
+</Router>
