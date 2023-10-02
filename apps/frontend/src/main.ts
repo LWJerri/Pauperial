2413a4527aa65268c688063c3f12f3a0bbe7e6f9
LWJerri <loginmanx@gmail.com>
2023-10-02 12:19:51 +0300

    Change main `new App()`

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,8 +1,6 @@
 import App from "./App.svelte";
 import "./index.css";
 
-const app = new App({
-  target: document.getElementById("app"),
-});
+const app = new App({ target: document.body });
 
 export default app;
