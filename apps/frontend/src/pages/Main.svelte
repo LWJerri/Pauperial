ae61304890feca37ab629f0191e4805a58b697fe
LWJerri <loginmanx@gmail.com>
2023-10-27 20:28:45 +0300

    Update `Main.svelte`

--- apps/frontend/src/pages/Main.svelte
+++ apps/frontend/src/pages/Main.svelte
@@ -2,18 +2,8 @@
   <div class="max-w-2xl rounded border-2 border-dashed border-cyan-500 p-2">
     <div class="mb-5 flex items-center justify-between">
       <h1 class="text-4xl font-bold outline-none">Pauperial</h1>
-
-      <a
-        href="https://github.com/LWJerri/Pauperial/issues/79"
-        target="_blank"
-        class="btn btn-sm btn-ghost align-right justify-end rounded">ROADMAP</a
-      >
     </div>
 
-    <p class="mb-5 text-xl">
-      Hello, dear user! Yes, this project is not dead and very soon I will release a new v3 version of this project.
-      <br /><br />Don't worry, all data will be saved (probably)... My last v2 version is seriously broken and does not
-      work anymore ;(
-    </p>
+    <p class="mb-5 text-xl">One day I will write this service from scratch and it will be even better!</p>
   </div>
 </div>
